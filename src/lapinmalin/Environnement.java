d9f456fff3fcb4521a7aad9911c8c8e9b68ad961
Matysiak Thomas <Matysiak Thomas>
2016-10-13 15:09:15 +0200

    modification gitignore, suppression des lapins et renards mort

--- src/lapinmalin/Environnement.java
+++ src/lapinmalin/Environnement.java
@@ -48,13 +48,21 @@ public class Environnement extends Observable {
 
     public void MiseAJour() {
         for (Lapin lapin : lapins) {
-            lapin.MiseAJourDirection(lapins);
-            lapin.MiseAJourPosition();
+            if (!lapin.estVivant())
+                lapins.remove(lapin);
+            else {
+                lapin.MiseAJourDirection(lapins);
+                lapin.MiseAJourPosition();
+            }
         }
 
         for (Renard renard : renards) {
-            renard.MiseAJourDirection(renards);
-            renard.MiseAJourPosition();
+            if (!renard.estVivant())
+                renards.remove(renard);
+            else {
+                renard.MiseAJourDirection(renards);
+                renard.MiseAJourPosition();
+            }
         }
         nbIterations++;
         if (nbIterations % 500 == 0) {
